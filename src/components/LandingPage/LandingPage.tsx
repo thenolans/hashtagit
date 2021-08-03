bf1794162d412bdc216d851d425719342c848d63
Tom Nolan <tsnolan@outlook.com>
2021-08-03 18:06:34 -0400

    chore: add bottom space to account for results

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -13,77 +13,80 @@ export default function LandingPage() {
   const history = useHistory();
 
   return (
-    <Container>
-      <div className="text-center space-y-8 py-8 text-gray-700">
-        <div className="text-5xl">
-          <Logo />
-        </div>
-        <p>
-          Quickly build and copy a list of hashtags for your social media posts
-          by selecting from your personalized database of commonly used tags!
-        </p>
-        {isAuthenticated ? (
-          <Button onClick={() => history.push(Urls.routes.app)}>
-            Go to app
-          </Button>
-        ) : (
-          <div className="space-x-4">
-            <Button onClick={() => loginWithRedirect()}>Login</Button>
-            <Button
-              onClick={() =>
-                loginWithRedirect({
-                  screen_hint: "signup",
-                })
-              }
-              theme="secondary"
-            >
-              Sign up
-            </Button>
+    <div className="pb-20">
+      <Container>
+        <div className="text-center space-y-8 py-8 text-gray-700">
+          <div className="text-5xl">
+            <Logo />
           </div>
-        )}
-        <hr />
-        <h3 className="text-2xl text-blue-700">Try it out!</h3>
-        <p>
-          Select any or all of the hashtags from the list below and click
-          "Results" to review and copy them to the clipboard. Create an account
-          to build your own lists!
-        </p>
-        <div className="text-left">
-          <HashtagProvider>
-            <div className="space-y-4">
-              <Category
-                sample
-                category={{
-                  _id: "sample1",
-                  name: "Dog posts",
-                  hashtags: [
-                    "dogs",
-                    "dogsofinstagram",
-                    "dogstagram",
-                    "siberianhusky",
-                    "husky",
-                  ],
-                }}
-              />
-              <Category
-                sample
-                category={{
-                  _id: "sample2",
-                  name: "Photography posts",
-                  hashtags: [
-                    "photography",
-                    "actionshot",
-                    "timelapse",
-                    "longexposure",
-                  ],
-                }}
-              />
+          <p>
+            Quickly build and copy a list of hashtags for your social media
+            posts by selecting from your personalized database of commonly used
+            tags!
+          </p>
+          {isAuthenticated ? (
+            <Button onClick={() => history.push(Urls.routes.app)}>
+              Go to app
+            </Button>
+          ) : (
+            <div className="space-x-4">
+              <Button onClick={() => loginWithRedirect()}>Login</Button>
+              <Button
+                onClick={() =>
+                  loginWithRedirect({
+                    screen_hint: "signup",
+                  })
+                }
+                theme="secondary"
+              >
+                Sign up
+              </Button>
             </div>
-            <HashtagPreview />
-          </HashtagProvider>
+          )}
+          <hr />
+          <h3 className="text-2xl text-blue-700">Try it out!</h3>
+          <p>
+            Select any or all of the hashtags from the list below and click
+            "Results" to review and copy them to the clipboard. Create an
+            account to build your own lists!
+          </p>
+          <div className="text-left">
+            <HashtagProvider>
+              <div className="space-y-4">
+                <Category
+                  sample
+                  category={{
+                    _id: "sample1",
+                    name: "Dog posts",
+                    hashtags: [
+                      "dogs",
+                      "dogsofinstagram",
+                      "dogstagram",
+                      "siberianhusky",
+                      "husky",
+                    ],
+                  }}
+                />
+                <Category
+                  sample
+                  category={{
+                    _id: "sample2",
+                    name: "Photography posts",
+                    hashtags: [
+                      "photography",
+                      "actionshot",
+                      "timelapse",
+                      "longexposure",
+                    ],
+                  }}
+                />
+              </div>
+              <HashtagPreview />
+            </HashtagProvider>
+          </div>
         </div>
-      </div>
-      <Footer />
-    </Container>
+        <Footer />
+      </Container>
+    </div>
   );
 }
