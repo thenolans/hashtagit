d949f24b0c0bcac7813152325e61caa1526285bf
Tom Nolan <tsnolan@outlook.com>
2025-07-10 18:39:58 -0400

    fix: autofocus with new category

--- src/components/CategoryForm/CategoryForm.tsx
+++ src/components/CategoryForm/CategoryForm.tsx
@@ -25,7 +25,6 @@ export default function CategoryForm({
 }: Props) {
   const nameInputRef = useRef<HTMLInputElement>(null);
   const lastHashtagInputRef = useRef<HTMLInputElement>(null);
-  const autoFocusHashtags = !!initialValues?.hashtags?.length;
 
   useEffect(() => {
     nameInputRef?.current?.focus();
@@ -46,102 +45,106 @@ export default function CategoryForm({
         });
       }}
       validationSchema={ValidationSchema}
-      render={({ values, handleChange, errors }) => (
-        <Card>
-          <Card.Header>
-            <div className="pl-4 mr-7">
-              <TextInput
-                autoFocus={!autoFocusHashtags}
-                name="name"
-                value={values.name}
-                onChange={handleChange}
-                placeholder="Category name..."
-                error={errors.name}
-                ref={nameInputRef}
-              />
-            </div>
-          </Card.Header>
-          <Card.Body>
-            <Form>
-              <div className="space-y-8">
-                <FieldArray
-                  name="hashtags"
-                  render={(arrayHelpers) => (
-                    <div className="space-y-2">
-                      {values.hashtags.map((_, index) => {
-                        const isOnlyHashtag = values.hashtags.length === 1;
-                        return (
-                          <div className="flex items-center" key={index}>
-                            <span
-                              aria-hidden="true"
-                              className="mr-2 text-lg text-gray-600"
-                            >
-                              #
-                            </span>
-                            <div className="mr-2 w-full">
-                              <TextInput
-                                autoFocus={autoFocusHashtags}
-                                name={`hashtags.${index}`}
-                                value={values.hashtags[index]}
-                                onChange={handleChange}
-                                onKeyDown={(e) => {
-                                  if (e.key === "Enter") {
-                                    e.preventDefault();
-                                    arrayHelpers.insert(
-                                      values.hashtags.length,
-                                      ""
-                                    );
-                                  }
-                                }}
-                                placeholder="Hashtag..."
-                                ref={
-                                  index === values.hashtags.length - 1
-                                    ? lastHashtagInputRef
-                                    : undefined
-                                }
-                              />
-                            </div>
-                            <Button
-                              className={
-                                isOnlyHashtag ? "invisible" : undefined
-                              }
-                              theme="tertiary"
-                              aria-label="Remove hashtag"
-                              aria-hidden={isOnlyHashtag}
-                              disabled={isOnlyHashtag}
-                              onClick={() => arrayHelpers.remove(index)}
-                            >
-                              <Icon
-                                size={16}
-                                className="text-red-600"
-                                icon="Trash"
-                              />
-                            </Button>
-                          </div>
-                        );
-                      })}
-                      <div className="text-center">
-                        <Button
-                          onClick={() => {
-                            arrayHelpers.insert(values.hashtags.length, "");
-                          }}
-                          theme="tertiary"
-                        >
-                          Add hashtag
-                        </Button>
-                      </div>
-                    </div>
-                  )}
+      render={({ values, handleChange, errors }) => {
+        const autoFocusHashtags = !!values.hashtags?.length;
+
+        return (
+          <Card>
+            <Card.Header>
+              <div className="pl-4 mr-7">
+                <TextInput
+                  autoFocus={!autoFocusHashtags}
+                  name="name"
+                  value={values.name}
+                  onChange={handleChange}
+                  placeholder="Category name..."
+                  error={errors.name}
+                  ref={nameInputRef}
                 />
-                <div className="flex justify-between items-center">
-                  <div>{additionalActions}</div>
-                  <Button type="submit">Done</Button>
-                </div>
               </div>
-            </Form>
-          </Card.Body>
-        </Card>
-      )}
+            </Card.Header>
+            <Card.Body>
+              <Form>
+                <div className="space-y-8">
+                  <FieldArray
+                    name="hashtags"
+                    render={(arrayHelpers) => (
+                      <div className="space-y-2">
+                        {values.hashtags.map((_, index) => {
+                          const isOnlyHashtag = values.hashtags.length === 1;
+                          return (
+                            <div className="flex items-center" key={index}>
+                              <span
+                                aria-hidden="true"
+                                className="mr-2 text-lg text-gray-600"
+                              >
+                                #
+                              </span>
+                              <div className="mr-2 w-full">
+                                <TextInput
+                                  autoFocus={autoFocusHashtags}
+                                  name={`hashtags.${index}`}
+                                  value={values.hashtags[index]}
+                                  onChange={handleChange}
+                                  onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                      e.preventDefault();
+                                      arrayHelpers.insert(
+                                        values.hashtags.length,
+                                        ""
+                                      );
+                                    }
+                                  }}
+                                  placeholder="Hashtag..."
+                                  ref={
+                                    index === values.hashtags.length - 1
+                                      ? lastHashtagInputRef
+                                      : undefined
+                                  }
+                                />
+                              </div>
+                              <Button
+                                className={
+                                  isOnlyHashtag ? "invisible" : undefined
+                                }
+                                theme="tertiary"
+                                aria-label="Remove hashtag"
+                                aria-hidden={isOnlyHashtag}
+                                disabled={isOnlyHashtag}
+                                onClick={() => arrayHelpers.remove(index)}
+                              >
+                                <Icon
+                                  size={16}
+                                  className="text-red-600"
+                                  icon="Trash"
+                                />
+                              </Button>
+                            </div>
+                          );
+                        })}
+                        <div className="text-center">
+                          <Button
+                            onClick={() => {
+                              arrayHelpers.insert(values.hashtags.length, "");
+                            }}
+                            theme="tertiary"
+                          >
+                            Add hashtag
+                          </Button>
+                        </div>
+                      </div>
+                    )}
+                  />
+                  <div className="flex justify-between items-center">
+                    <div>{additionalActions}</div>
+                    <Button type="submit">Done</Button>
+                  </div>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        );
+      }}
     />
   );
 }
